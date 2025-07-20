cef6e451db7db315e4ccfadc363b5fa15a129606
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-07-20 15:09:40 -0500

    ajustando conf y asgi de vercel

--- citas_profesionales/asgi.py
+++ citas_profesionales/asgi.py
@@ -5,5 +5,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citas_profesionales.settings')
 
 application = get_asgi_application()
-
-app = application
