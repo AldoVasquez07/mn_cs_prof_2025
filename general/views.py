a317350a29a5511ccdae56dc2ef1b5e517495689
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-07-22 16:07:16 -0500

    Ajustando aarchivos statics y redirecciones, además de flujo de urls

--- general/views.py
+++ general/views.py
@@ -5,7 +5,6 @@ def main_content_page(request):
     return render(request, 'general/main_page_citas.html')
 
 
-
 def seleccion_tipo_usuario(request):
     return render(request, 'general/seleccion_tipo_usuario.html')
 
